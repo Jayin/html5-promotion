4c3bebe3b2f6aab4227b07ab306ebc041d7297d4
zhlhuang <zhlhuang888@foxmail.com>
2016-02-22 10:46:39 +0800

    #1 搭建基本的样式结构

--- Application/Home/Controller/IndexController.class.php
+++ Application/Home/Controller/IndexController.class.php
@@ -4,6 +4,8 @@ use Think\Controller;
 
 class IndexController extends Controller {
 	public function index() {
+		$this->assign('homePage', "active"); //菜单选择样式
+		$this->assign('title', "主页"); //标题显示
 		$this->display("index");
 	}
 }
