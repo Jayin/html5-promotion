21d80b4695b41a4926366da860cfab76977a9dcc
Jayin Ton <tonjayin@gmail.com>
2016-03-04 16:15:57 +0800

    plugin input: 支持多个一次性提交

--- Application/Common/Controller/PluginController.class.php
+++ Application/Common/Controller/PluginController.class.php
@@ -10,12 +10,34 @@ namespace Common\Controller;
 
 
 use Think\Controller;
+use Home\Service\PluginService;
+use Home\Service\ProjectService;
 
 class PluginController extends Controller {
 
     public function update() {
+        $project_name = I('post.project_name');
+        $plugin_name = I('post.plugin_name');
+        $plugin_input_names = I('post.plugin_input_names');
+        $values = array();
+        foreach ($plugin_input_names as $index => $plugin_input_name) {
+            $values[] = I('post.value_' . $plugin_input_name, '', '');
+        }
 
+        if (!$project_name || !$plugin_name || !$plugin_input_names) {
+            $this->error("请检查参数");
 
+            return;
+        }
+        $project_plugin_config = ProjectService::readProjectDevPluginConfig($project_name);
+        $plugin_config = $project_plugin_config['config'][$plugin_name];
+
+        foreach ($plugin_config['files'] as $index => $file) {
+            //更新配置后再渲染
+            PluginService::batchUpdatePluginConfigRegex($project_name, $plugin_name, $plugin_input_names, $values);
+            ProjectService::renderFile($project_name, $file);
+        }
+        $this->success('更新成功');
     }
 
 }
--- Application/Plugin/Controller/ZhutibangCopyrightController.class.php
+++ Application/Plugin/Controller/ZhutibangCopyrightController.class.php
@@ -10,46 +10,12 @@ namespace Plugin\Controller;
 
 use Common\Controller\PluginController;
 
-use Home\Service\PluginService;
-use Home\Service\ProjectService;
-
 /**
  * Class ZhutibangCopyrightController
  * @package Plugin\Controller
  */
 class ZhutibangCopyrightController extends PluginController{
 
-    public function update() {
-        $project_name = I('post.project_name');
-        $plugin_name = I('post.plugin_name');
-        $plugin_input_name = I('post.plugin_input_name');
-        $value = I('post.value');
 
-        if (!$project_name || !$plugin_name || !$plugin_input_name) {
-            $this->error("请检查参数");
-            return;
-        }
-        $project_plugin_config = ProjectService::readProjectDevPluginConfig($project_name);
-        $plugin_config = $project_plugin_config['config'][$plugin_name];
-        $plugin_input = null;
-        foreach ($plugin_config['input'] as $index => $input) {
-            if ($input['name'] == $plugin_input_name) {
-                $plugin_input = $input;
-                $plugin_config['input'][$index]['value'] = $value;
-                break;
-            }
-        }
-        if (!$plugin_input) {
-            $this->error("找不到配置");
-            return;
-        }
-
-        foreach ($plugin_config['files'] as $index => $file) {
-            //输入什么 就是什么值，最终$plugin_input的值取决于各个$value的统计方式
-            PluginService::updatePluginConfigRegex($project_name, $plugin_name, $plugin_input_name, $value);
-            ProjectService::renderFile($project_name, $file);
-        }
-        $this->success('更新成功');
-    }
 
 }
